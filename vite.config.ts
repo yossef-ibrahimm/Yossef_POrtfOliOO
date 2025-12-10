f1caa9617edfa9a9a3d129072255a9712d2e53a5
yossef-ibrahimm <yossef.ibrahimm@gmail.com>
2025-12-10 14:06:22 +0200

    update config and deploy setup

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,21 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/Yossef_POrtfOliOO/',   // ✅ هنا في المكان الصحيح
+
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
+  plugins: [
+    react(),
+    mode === "development" && componentTagger()
+  ].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-     base: '/Yossef_POrtfOliOO/',
   },
 }));
